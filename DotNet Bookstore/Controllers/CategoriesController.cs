e0b5c1d87d812c2a69b9bfdedb631f31a8568d6b
Dario-Hesami <Dario.Academic@gmail.com>
2024-09-13 13:52:59 -0400

    Week 2 - Complete

---
+++ DotNet Bookstore/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_Bookstore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Browse(string category)
+        {
+            ViewBag.Category = category;
+            return View();
+        }
+    }
+}
