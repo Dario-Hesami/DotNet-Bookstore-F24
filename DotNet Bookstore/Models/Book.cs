3d699750727288fe284c6cf76ba86bcc32dadc67
Dario Hesami <Hossein.Hesami@GeorgianCollege.ca>
2024-09-27 13:55:04 -0400

    Week 2 - Complete

---
+++ DotNet Bookstore/Models/Book.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_Bookstore.Models
+{
+    public class Book
+    {
+        //PK
+
+        public int BookId { get; set; }
+
+
+
+        [Required]
+
+        [MaxLength(100)]
+
+        public string Author { get; set; }
+
+
+
+        [Required]
+
+        public string Title { get; set; }
+
+
+
+        public string? Image { get; set; }
+
+
+
+        [Required]
+
+        [Range(0.01, 999999)]
+
+        public decimal Price { get; set; }
+
+
+
+        public bool MatureContent { get; set; }
+
+
+
+        //FK
+
+        public int CategoryId { get; set; }
+
+
+
+        // child references
+
+        public List<CartItem>? CartItems { get; set; }
+
+        public List<OrderDetail>? OrderDetails { get; set; }
+
+
+
+        // parent reference
+
+        public Category? Category { get; set; }
+    }
+}
--- DotNet Bookstore/Models/Category.cs
+++ DotNet Bookstore/Models/Category.cs
@@ -5,10 +5,14 @@ namespace DotNet_Bookstore.Models
     public class Category
     {
         // pk fields should always be called either {Model}Id or just Id
-        [Display(Name = "Category ID")]
-        [Range(1,999999)]
+        //[Display(Name = "Category ID")]
+        //[Range(1,999999)]
         public int CtegoryId { get; set; }
         [Required(ErrorMessage = "A customized error message")]
         public string Name { get; set; }
+
+        // child reference to Books (1 Category / Many Books)
+
+        public List<Book>? Books { get; set; }
     }
 }
