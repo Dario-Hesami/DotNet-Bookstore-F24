c5622fcae8cdb48272fedab04e0224778472a5c7
Dario Hesami <Hossein.Hesami@GeorgianCollege.ca>
2024-11-07 20:07:48 -0500

    1- Scaffold Register.cshtml.cs and modify it | 2- Modify Program.cs (to add new users in the Customer role)

--- DotNet Bookstore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DotNet Bookstore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -84,7 +84,7 @@ namespace DotNet_Bookstore.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -120,6 +120,9 @@ namespace DotNet_Bookstore.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    // automatically put any new users into the role of Customer => saves to AspNetUserRoles
+                    await _userManager.AddToRoleAsync(user, "Customer");
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
