1cb5555e80ff95c77fb6d9d163ecb0fc5a9278be
Dario Hesami <Hossein.Hesami@GeorgianCollege.ca>
2024-11-08 11:57:19 -0500

    Week 9 - P1 - Shop Controller

---
+++ DotNet Bookstore/Controllers/ShopController.cs
@@ -0,0 +1,21 @@
+﻿using DotNet_Bookstore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_Bookstore.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // query the list of categories in a-z order for display
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}
