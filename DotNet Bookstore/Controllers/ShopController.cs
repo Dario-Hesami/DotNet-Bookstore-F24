d2f11bdce2beec59bede33775b821413b83411d8
Dario Hesami <Hossein.Hesami@GeorgianCollege.ca>
2024-11-15 12:22:53 -0500

    Week 10 - P1 - Add ShopByCategory action methods and views

--- DotNet Bookstore/Controllers/ShopController.cs
+++ DotNet Bookstore/Controllers/ShopController.cs
@@ -17,5 +17,30 @@ namespace DotNet_Bookstore.Controllers
             var categories = _context.Categories.OrderBy(c => c.Name).ToList();
             return View(categories);
         }
+
+        // GET: /Shop/ShopByCategory/6 
+
+        public IActionResult ShopByCategory(int id)
+
+        {
+
+            // Display the category name on the page based on the ID - store category name in the ViewData object 
+            var category = _context.Categories.Find(id);
+
+            // return to /shop/index if category not found 
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewData["Category"] = category.Name;
+
+            // query the Books filtered by the selected CategoryId param 
+            var books = _context.Books.Where(b => b.CategoryId == id)
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            // send the Books list to the view for display 
+            return View(books);
+        }
     }
 }
