60e2e37301621ce64920507ea26cd613e4ad421d
Dario Hesami <Hossein.Hesami@GeorgianCollege.ca>
2024-11-15 00:01:35 -0500

    Delete appsettings.json

--- DotNet Bookstore/Program.cs
+++ DotNet Bookstore/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
