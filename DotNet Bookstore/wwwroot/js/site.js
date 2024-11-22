53911644077efe2ceebc05a3e2f13f5732ba653c
Dario Hesami <Hossein.Hesami@GeorgianCollege.ca>
2024-11-22 12:08:53 -0500

    Week 11 - P1 - start the Checkout steps

--- DotNet Bookstore/wwwroot/js/site.js
+++ DotNet Bookstore/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function confirmDelete() {
+    return (confirm("Are you sure you want to remove this item?"));
+}
